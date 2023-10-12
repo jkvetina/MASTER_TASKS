b5743d70dd8189d2ddfccf09cc75676a8a613d3a
Jan Kvetina <jan.kvetina@gmail.com>
2023-10-12 19:17:02 +0200

    Cleaner fn. for links

--- database/packages/tsk_nav.sql
+++ database/packages/tsk_nav.sql
@@ -1,5 +1,63 @@
 CREATE OR REPLACE PACKAGE BODY tsk_nav AS
 
+    FUNCTION get_link (
+        in_content          VARCHAR2,
+        in_page_id          NUMBER,
+        in_card_id          tsk_cards.card_id%TYPE          := NULL,
+        in_client_id        tsk_recent.client_id%TYPE       := NULL,
+        in_project_id       tsk_recent.project_id%TYPE      := NULL,
+        in_board_id         tsk_recent.board_id%TYPE        := NULL,
+        in_swimlanes        tsk_recent.swimlanes%TYPE       := NULL,
+        in_owners           tsk_recent.owners%TYPE          := NULL,
+        in_items            VARCHAR2                        := NULL,
+        in_values           VARCHAR2                        := NULL,
+        in_class            VARCHAR2                        := NULL,
+        in_icon_name        VARCHAR2                        := NULL
+    )
+    RETURN VARCHAR2
+    AS
+        v_items             VARCHAR2(32767);
+        v_values            VARCHAR2(32767);
+        v_icon              VARCHAR2(32767);
+    BEGIN
+        --
+        IF in_client_id IS NOT NULL THEN
+            v_items         := v_items  || ',P0_CLIENT_ID';
+            v_values        := v_values || ',' || in_client_id;
+        END IF;
+        --
+        IF in_project_id IS NOT NULL THEN
+            v_items         := v_items  || ',P0_PROJECT_ID';
+            v_values        := v_values || ',' || in_project_id;
+        END IF;
+        --
+        IF in_board_id IS NOT NULL THEN
+            v_items         := v_items  || ',P0_BOARD_ID';
+            v_values        := v_values || ',' || in_board_id;
+        END IF;
+        --
+        v_icon := CASE
+            WHEN in_icon_name IS NOT NULL
+                THEN '<span class="fa ' || in_icon_name || '"></span><span> &' || 'nbsp; '
+            ELSE '<span>&' || 'mdash;&' || 'nbsp; '
+            END;
+        --
+        RETURN '<a href="' ||
+            APEX_PAGE.GET_URL (
+                --p_application   =>
+                p_page          => COALESCE(in_page_id, core.get_page_id()),
+                --p_session       =>
+                --p_request       =>
+                --p_debug         =>
+                p_clear_cache   => COALESCE(in_page_id, core.get_page_id()),
+                p_items         => SUBSTR(v_items,  2, 4000),
+                p_values        => SUBSTR(v_values, 2, 4000)
+            ) ||
+            '" class="' || in_class || '">' || v_icon || in_content || '</span></a>';
+    END;
+
+
+
     FUNCTION get_home
     RETURN VARCHAR2             -- 32k limit! - well, actually 4k limit on NAV view
     AS
@@ -9,6 +67,7 @@ CREATE OR REPLACE PACKAGE BODY tsk_nav AS
         last_client     tsk_projects.client_id%TYPE     := '-';
         last_project    tsk_projects.project_id%TYPE    := '-';
     BEGIN
+        -- show favorite/bookmarked boards
         o := o || '<div class="M1"><span class="fa fa-heart-o"></span> &' || 'nbsp; <span>Favorites</span></div>';
         o := o || '<div>';      -- class=ROW
         --
@@ -28,40 +87,52 @@ CREATE OR REPLACE PACKAGE BODY tsk_nav AS
             WHERE b.is_favorite = 'Y'
             ORDER BY b.client_name, b.project_name, b.board_name
         ) LOOP
-            -- render client
+            -- render client link
             IF b.client_id != last_client THEN
-                o := o || tsk_app.get_link (
-                    CASE WHEN b.client_id = tsk_app.get_client_id() THEN '<span class="fa fa-arrow-circle-right"></span><span>' ELSE '<span>&' || 'mdash;' END ||
-                    '&' || 'nbsp; ' || b.client_name || '</span>',
-                    b.client_id,
-                    in_class => 'M2'
+                o := o || get_link (
+                    in_content      => b.client_name,
+                    in_page_id      => 100,
+                    in_client_id    => b.client_id,
+                    in_project_id   => NULL,
+                    in_board_id     => NULL,
+                    in_swimlanes    => NULL,
+                    in_owners       => NULL,
+                    in_class        => 'M2',
+                    in_icon_name    => CASE WHEN b.client_id = tsk_app.get_client_id() THEN 'fa-arrow-circle-right' END
                 );
                 --
                 last_client := b.client_id;
             END IF;
 
-            -- render project
+            -- render project link
             IF b.project_id != last_project THEN
-                o := o || tsk_app.get_link (
-                    CASE WHEN b.project_id = tsk_app.get_project_id() THEN '<span class="fa fa-arrow-circle-right"></span><span>' ELSE '<span>&' || 'mdash;' END ||
-                    '&' || 'nbsp; ' || b.project_name || '</span>',
-                    b.client_id,
-                    b.project_id,
-                    in_class => 'M3' || CASE WHEN b.boards = 1 THEN REPLACE(b.is_current, 'Y', ' ACTIVE') END
+                o := o || get_link (
+                    in_content      => b.project_name,
+                    in_page_id      => 100,
+                    in_client_id    => b.client_id,
+                    in_project_id   => b.project_id,
+                    in_board_id     => NULL,
+                    in_swimlanes    => NULL,
+                    in_owners       => NULL,
+                    in_class        => 'M3' || CASE WHEN b.boards = 1 THEN REPLACE(b.is_current, 'Y', ' ACTIVE') END,
+                    in_icon_name    => CASE WHEN b.project_id = tsk_app.get_project_id() THEN 'fa-arrow-circle-right' END
                 );
                 --
                 last_project := b.project_id;
             END IF;
 
-            -- render board
+            -- render board link
             IF b.boards > 1 THEN
-                o := o || tsk_app.get_link (
-                    CASE WHEN b.is_current = 'Y' THEN '<span class="fa fa-arrow-circle-right"></span><span>' ELSE '<span>&' || 'mdash;' END ||
-                    '&' || 'nbsp; ' || b.board_name || '</span>',
-                    b.client_id,
-                    b.project_id,
-                    b.board_id,
-                    in_class => 'M4' || REPLACE(b.is_current, 'Y', ' ACTIVE')
+                o := o || get_link (
+                    in_content      => b.board_name,
+                    in_page_id      => 100,
+                    in_client_id    => b.client_id,
+                    in_project_id   => b.project_id,
+                    in_board_id     => b.board_id,
+                    in_swimlanes    => NULL,
+                    in_owners       => NULL,
+                    in_class        => 'M4' || REPLACE(b.is_current, 'Y', ' ACTIVE'),
+                    in_icon_name    => CASE WHEN b.is_current = 'Y' THEN 'fa-arrow-circle-right' END
                 );
             END IF;
         END LOOP;
@@ -72,6 +143,9 @@ CREATE OR REPLACE PACKAGE BODY tsk_nav AS
         -- add recent tasks
         FOR t IN (
             SELECT
+                t.client_id,
+                t.project_id,
+                t.board_id,
                 t.card_id,
                 t.card_number,
                 t.card_name,
@@ -90,12 +164,16 @@ CREATE OR REPLACE PACKAGE BODY tsk_nav AS
             ORDER BY t.updated_at DESC
             FETCH FIRST 10 ROWS ONLY
         ) LOOP
-            t.card_name := CASE WHEN LENGTH(t.card_name) > 30 THEN SUBSTR(TRIM(t.card_name), 1, 27) || '...' ELSE t.card_name END;
-            r := r || '<a href="' ||
-                tsk_app.get_card_link(t.card_id) ||
-                '" class="M2">' ||
-                CASE WHEN t.badge BETWEEN 1 AND 5 THEN '<span class="fa fa-number-' || t.badge || '"></span><span>' ELSE '<span>&' || 'mdash;' END ||
-                '&' || 'nbsp; ' || NVL(t.card_number, '#' || t.card_id) || ' - ' || t.card_name || '</span></a>';
+            r := r || get_link (
+                in_content      => NVL(t.card_number, '#' || t.card_id) || ' - ' || CASE WHEN LENGTH(t.card_name) > 30 THEN SUBSTR(TRIM(t.card_name), 1, 27) || '...' ELSE t.card_name END,
+                in_page_id      => 105,
+                in_card_id      => t.card_id,
+                in_client_id    => t.client_id,
+                in_project_id   => t.project_id,
+                in_board_id     => t.board_id,
+                in_class        => 'M2',
+                in_icon_name    => CASE WHEN t.badge BETWEEN 1 AND 5 THEN 'fa-number-' || t.badge END
+            );
         END LOOP;
         --
         o := o || '</div>';
@@ -278,8 +356,6 @@ CREATE OR REPLACE PACKAGE BODY tsk_nav AS
             RETURN NULL;
         END IF;
         --
-        o := o || '<div class="ROW">';
-        o := o || '<div>';
         o := o || '<div class="M1"><span class="fa fa-chevron-down"></span> &' || 'nbsp; <span>Select Board</span></div>';
         --
         FOR t IN (
@@ -302,7 +378,6 @@ CREATE OR REPLACE PACKAGE BODY tsk_nav AS
             );
         END LOOP;
         --
-        o := o || '</div>';
         --
         RETURN
             '<div>' || o || '</div>';
