9572b065a6af7fa18ef76c647e28b0c3bfe4034a
Jan Kvetina <jan.kvetina@gmail.com>
2023-10-25 21:52:05 +0200

    Nav styles

--- database/apex/f710/application/pages/page_00400.sql
+++ database/apex/f710/application/pages/page_00400.sql
@@ -18,6 +18,7 @@ wwv_flow_imp_page.create_page(
 ,p_step_title=>'Boards'
 ,p_autocomplete_on_off=>'OFF'
 ,p_group_id=>wwv_flow_imp.id(23490292620162217)  -- 4) BOARDS
+,p_page_css_classes=>'MULTICOLUMN'
 ,p_page_template_options=>'#DEFAULT#'
 ,p_required_role=>wwv_flow_imp.id(70314822393792529)  -- MASTER - IS_USER
 ,p_protection_level=>'C'
