53d480d21245e96e75ffb7e8cbc760602dc64801
Jan Kvetina <jan.kvetina@gmail.com>
2023-10-26 17:22:46 +0200

    Cleanup

--- database/packages/tsk_nav.spec.sql
+++ database/packages/tsk_nav.spec.sql
@@ -34,26 +34,6 @@ CREATE OR REPLACE PACKAGE tsk_nav AS
 
 
 
-    FUNCTION get_swimlanes
-    RETURN VARCHAR2;
-
-
-
-    FUNCTION get_statuses
-    RETURN VARCHAR2;
-
-
-
-    FUNCTION get_categories
-    RETURN VARCHAR2;
-
-
-
-    FUNCTION get_owners
-    RETURN VARCHAR2;
-
-
-
     FUNCTION get_commits
     RETURN VARCHAR2;
 
