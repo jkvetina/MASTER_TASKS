e4fdf8668340ac92a1b2fc7650405f27c22c924f
Jan Kvetina <jan.kvetina@gmail.com>
2023-10-30 07:56:28 +0100

    Fix missing auth

--- database/apex/f710/application/pages/page_00360.sql
+++ database/apex/f710/application/pages/page_00360.sql
@@ -20,6 +20,7 @@ wwv_flow_imp_page.create_page(
 ,p_group_id=>wwv_flow_imp.id(78006013489194627)  -- 3) PROJECTS
 ,p_page_css_classes=>'MULTICOLUMN'
 ,p_page_template_options=>'#DEFAULT#'
+,p_required_role=>wwv_flow_imp.id(70314822393792529)  -- MASTER - IS_USER
 ,p_protection_level=>'C'
 ,p_page_component_map=>'11'
 ,p_last_updated_by=>'DEV'
