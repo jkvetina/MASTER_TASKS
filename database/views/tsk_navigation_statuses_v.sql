b88db652d92316006af430bc8f5bc863416fb166
Jan Kvetina <jan.kvetina@gmail.com>
2023-10-31 21:56:09 +0100

    Show status groups in Nav

--- database/views/tsk_navigation_statuses_v.sql
+++ database/views/tsk_navigation_statuses_v.sql
@@ -20,6 +20,14 @@ counts AS (
     GROUP BY
         c.status_id
 ),
+a AS (
+    SELECT /*+ MATERIALIZE */
+        t.status_group
+    FROM tsk_lov_statuses_v t
+    WHERE t.status_group IS NOT NULL
+    GROUP BY
+        t.status_group
+),
 filter_data AS (
     SELECT
         2 AS lvl,
@@ -27,11 +35,12 @@ filter_data AS (
         tsk_nav.get_link (
             in_content      => 'All Statuses',
             in_page_id      => core.get_page_id(),
-            in_status_id    => '-',
+            in_status_id  => '-',
             in_class        => '',
             in_icon_name    => ''
         ) AS attribute01,
         --
+        '' AS attribute08,
         ' class="NAV_L3"' AS attribute10,
         --
         x.endpoint || '/0/.' AS order#
@@ -43,22 +52,60 @@ filter_data AS (
         2 AS lvl,
         --
         tsk_nav.get_link (
+            in_content      => 'No status',
+            in_page_id      => core.get_page_id(),
+            in_status_id  => '!',
+            in_class        => '',
+            in_icon_name    => CASE WHEN x.status_id = '!' THEN 'fa-arrow-circle-right' END,
+            in_badge        => c.row_count
+        ) AS attribute01,
+        --
+        '' AS attribute08,
+        ' class="NAV_L3' || CASE WHEN x.status_id = '!' THEN ' ACTIVE' END || '"' AS attribute10,
+        --
+        x.endpoint || '/0/..' AS order#
+        --
+    FROM x
+    LEFT JOIN counts c
+        ON 1 = 1
+        AND c.status_id   IS NULL
+    UNION ALL
+    --
+    SELECT
+        2 AS lvl,
+        --
+        '<span>' || a.status_group || '</span>' AS attribute01,
+        --
+        '</ul><ul>'         AS attribute08,     -- start new column with each group
+        ' class="NAV_L2"'   AS attribute10,
+        --
+        x.endpoint || '/0/' || a.status_group AS order#
+        --
+    FROM a
+    CROSS JOIN x
+    --
+    UNION ALL
+    SELECT
+        2 AS lvl,
+        --
+        tsk_nav.get_link (
             in_content      => a.status_name,
             in_page_id      => core.get_page_id(),
-            in_status_id    => a.status_id,
+            in_status_id  => a.status_id,
             in_class        => '',
             in_icon_name    => CASE WHEN x.status_id = a.status_id THEN 'fa-arrow-circle-right' END,
             in_badge        => c.row_count
         ) AS attribute01,
         --
+        '' AS attribute08,
         ' class="NAV_L3' || CASE WHEN x.status_id = a.status_id THEN ' ACTIVE' END || '"' AS attribute10,
         --
-        x.endpoint || '/0/' || a.order# AS order#
+        x.endpoint || '/0/' || a.status_group || '.' || a.order# AS order#
         --
     FROM tsk_lov_statuses_v a
     CROSS JOIN x
     LEFT JOIN counts c
-        ON c.status_id      = a.status_id
+        ON c.status_id    = a.status_id
 )
 SELECT
     2 AS lvl,
@@ -91,11 +138,10 @@ SELECT
     '' AS attribute05,
     '' AS attribute06,
     '' AS attribute07,
-    '' AS attribute08,
+    t.attribute08,
     '' AS attribute09,
-    --
     t.attribute10,
-    t.order#
+    t.order# AS order#
     --
 FROM filter_data t;
 --
