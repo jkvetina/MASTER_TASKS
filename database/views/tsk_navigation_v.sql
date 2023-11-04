6cfe9d889ed3e9cf8b40a4d1f04d13ca0062c801
Jan Kvetina <jan.kvetina@gmail.com>
2023-11-04 07:26:42 +0100

    Remove unused nav views

--- database/views/tsk_navigation_v.sql
+++ database/views/tsk_navigation_v.sql
@@ -32,27 +32,5 @@ UNION ALL
 --
 SELECT
     t.lvl, t.attribute01, t.attribute02, t.attribute03, t.attribute04, t.attribute05, t.attribute06, t.attribute07, t.attribute08, t.attribute09, t.attribute10, t.order#
-FROM tsk_navigation_boards_v t
-UNION ALL
---
-SELECT
-    t.lvl, t.attribute01, t.attribute02, t.attribute03, t.attribute04, t.attribute05, t.attribute06, t.attribute07, t.attribute08, t.attribute09, t.attribute10, t.order#
-FROM tsk_navigation_swimlanes_v t
-UNION ALL
---
-SELECT
-    t.lvl, t.attribute01, t.attribute02, t.attribute03, t.attribute04, t.attribute05, t.attribute06, t.attribute07, t.attribute08, t.attribute09, t.attribute10, t.order#
-FROM tsk_navigation_statuses_v t
-UNION ALL
---
-SELECT
-    t.lvl, t.attribute01, t.attribute02, t.attribute03, t.attribute04, t.attribute05, t.attribute06, t.attribute07, t.attribute08, t.attribute09, t.attribute10, t.order#
-FROM tsk_navigation_categories_v t
-UNION ALL
---
-SELECT
-    t.lvl, t.attribute01, t.attribute02, t.attribute03, t.attribute04, t.attribute05, t.attribute06, t.attribute07, t.attribute08, t.attribute09, t.attribute10, t.order#
-FROM tsk_navigation_owners_v t;
---
-COMMENT ON TABLE tsk_navigation_v IS '';
+FROM tsk_navigation_boards_v t;
 
